ed3671b8b78e2bae9fa594bd21d31fec56074667
Javenkm <Javenkm@gmail.com>
2021-05-20 18:34:08 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import Pokemon from "./components/Pokemon"
 import './App.css';
-import axios from 'axios';
 
 
 function App() {
 
   const [ pokemon, setPokemon ] = useState({});
 
-  const getPokemon = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
-      .then(res => {
-        setPokemon(res.data.results)
-      })
-  }
-
-
-  // const getPokemon = () => {
-  //   fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+  // useEffect(() => {
+  //   fetch('https://pokeapi.co/api/v2/pokemon/')
   //     .then(res => res.json())
   //     .then(res => setPokemon(res.results))
-  //     .catch(err => console.log(err))
-  // }
+  // }, []);
+
+
+  const getPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(res => res.json())
+      .then(res => setPokemon(res.results))
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className="parent-div">
@@ -34,13 +32,12 @@ function App() {
           Catch 'Em All
         </button>
       </div>
-      { pokemon ?
-        pokemon.map((p, i) => {
+      { pokemon.map((p, i) => {
           return <Pokemon
                     pokemon = {p}
                     key = {i}
                   />
-        }) : <div></div>
+        })
       }
     </div>
   );
